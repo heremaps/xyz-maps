b5680ecbe3ca1935afdf0a9d7044f0f52a62f00a
Tim Deubler <tim.deubler@here.com>
2020-02-13 16:06:37 +0100

    updated publish script
    Signed-off-by: Tim Deubler <tim.deubler@here.com>

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -2,7 +2,7 @@
 # simple script for npm publishing
 set -ex
 
-#echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > ~/.npmrc
+echo '//registry.npmjs.org/:_authToken=${NPM_TOKEN}' > ~/.npmrc
 
 yarn build-release
 
@@ -14,8 +14,8 @@ cd packages
 for pkg in "${packages[@]}"
 do
     cd "$pkg"
-    npm publish --dry-run $1
+    npm publish $1
     cd -
 done
 
-echo "\npublished $1 to npm sucessfully!\n"
+echo "published $1 all packages to npm successfully!"
