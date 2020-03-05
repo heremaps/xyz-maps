153efd5d88da1743b1501225ec41b26725d9721a
Tim Deubler <tim.deubler@here.com>
2020-03-05 09:45:14 +0100

    fixed: bundling issues when webpack is used to consume packages
    Signed-off-by: Tim Deubler <tim.deubler@here.com>

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,4 +1,21 @@
-// rollup.config.js
+/*
+ * Copyright (C) 2019-2020 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
 
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
@@ -98,25 +115,5 @@ const rollupConfig = [{
     ]
 }];
 
-if (production) {
-    rollupConfig.push({
-        input: 'build/bundle.js',
-        output: {
-            name: 'here.xyz.maps',
-            extend: true,
-            file: 'dist/' + file.replace('.', '.esm.'),
-            format: 'esm',
-            sourcemap: sourcemap,
-            banner: banner,
-            globals: globals,
-            intro: intro
-        },
-        external: external,
-        treeshake: false,
-        plugins: [
-            sourcemaps()
-        ]
-    });
-}
 
 export default rollupConfig;
