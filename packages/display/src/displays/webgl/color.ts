c1196391eb35b4eb683152d2d03c110b88a1d945
Tim Deubler <tim.deubler@here.com>
2020-06-18 13:13:11 +0200

    added(display): styling support of 3-digit hex color codes
    Signed-off-by: Tim Deubler <tim.deubler@here.com>

--- packages/display/src/displays/webgl/color.ts
+++ packages/display/src/displays/webgl/color.ts
@@ -170,12 +170,19 @@ const HTML_COLOR_NAMES = {
 };
 
 
-export const hexToRGBA = (hex: string): RGBA => [
-    parseInt(hex.slice(1, 3), 16) / 255,
-    parseInt(hex.slice(3, 5), 16) / 255,
-    parseInt(hex.slice(5, 7), 16) / 255,
-    1
-];
+export const hexToRGBA = (hex: string): RGBA => {
+    return hex.length < 5 ? [
+        parseInt(`${hex[1]}${hex[1]}`, 16) / 255,
+        parseInt(`${hex[2]}${hex[2]}`, 16) / 255,
+        parseInt(`${hex[3]}${hex[3]}`, 16) / 255,
+        1
+    ] : [
+        parseInt(hex.slice(1, 3), 16) / 255,
+        parseInt(hex.slice(3, 5), 16) / 255,
+        parseInt(hex.slice(5, 7), 16) / 255,
+        1
+    ];
+};
 
 for (let name in HTML_COLOR_NAMES) {
     HTML_COLOR_NAMES[name] = hexToRGBA('#' + HTML_COLOR_NAMES[name]);
