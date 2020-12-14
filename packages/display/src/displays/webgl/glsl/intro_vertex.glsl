1eb5845ccc7ada269fed5eea706dbdbe1421307d
Tim Deubler <tim.deubler@here.com>
2020-12-14 16:43:09 +0100

    tile boundaries might flickering on retina
    Signed-off-by: Tim Deubler <tim.deubler@here.com>

--- packages/display/src/displays/webgl/glsl/intro_vertex.glsl
+++ packages/display/src/displays/webgl/glsl/intro_vertex.glsl
@@ -5,6 +5,7 @@ vec2 round(vec2 point){
 }
 
 vec4 snapToScreenPixel(vec4 position, vec2 resolution){
+    resolution *= DEVICE_PIXEL_RATIO;
     vec2 screenPixel = ((position.xy / position.w + 1.0) / 2.0) * resolution;
     position.xy = (round(screenPixel) / resolution * 2.0 - 1.0) * position.w;
     return position;
