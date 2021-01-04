a6a789a6acb4c1ac22b35d0694398c883cc470ba
Tim Deubler <tim.deubler@here.com>
2021-01-04 16:49:36 +0100

    fixed(display): strokeWidth is used to determine the correct size of Circles
    Signed-off-by: Tim Deubler <tim.deubler@here.com>

--- packages/display/src/displays/webgl/glsl/circle_fragment.glsl
+++ packages/display/src/displays/webgl/glsl/circle_fragment.glsl
@@ -2,20 +2,19 @@ precision lowp float;
 
 uniform vec4 u_fill;
 uniform vec4 u_stroke;
-uniform float u_radius;
 uniform float u_strokeWidth;
 
-varying vec2 vDir;
+varying float v_radius;
+varying vec2 v_position;
 
 #define COLOR_UNDEF -1.0
 
 void main(void){
+    float r = length(v_position);
 
-    float r = length(vDir);
+    if (r > v_radius) discard;
 
-    if (r > u_radius) discard;
-
-    if (r < u_radius - u_strokeWidth){
+    if (r < v_radius - u_strokeWidth){
         if(u_fill[0] == COLOR_UNDEF) discard;
         gl_FragColor = u_fill;
     } else {
--- packages/display/src/displays/webgl/glsl/circle_vertex.glsl
+++ packages/display/src/displays/webgl/glsl/circle_vertex.glsl
@@ -8,8 +8,10 @@ uniform vec2 u_offset;
 uniform float u_scale;
 uniform vec2 u_resolution;
 uniform bool u_alignMap;
+uniform float u_strokeWidth;
 
-varying vec2 vDir;
+varying vec2 v_position;
+varying float v_radius;
 
 const float EXTENT_SCALE = 1.0 / 32.0; // 8912->512
 
@@ -18,14 +20,17 @@ void main(void){
     vec2 dir = mod(a_position, 2.0) * 2.0 - 1.0;
     vec2 pos = floor(a_position * .5) * EXTENT_SCALE;
 
+    float radius = u_radius + u_strokeWidth / 2.0;
+
+    v_position = dir * radius;
+    v_radius = radius;
+
     if (u_alignMap){
-        vec2 shift = (u_offset + dir * u_radius) / u_scale;
+        vec2 shift = (u_offset + v_position) / u_scale;
         gl_Position = u_matrix * vec4(u_topLeft + pos + shift, 0.0, 1.0);
     } else {
         vec4 cpos = u_matrix * vec4(u_topLeft + pos, 0.0, 1.0);
         vec2 offset = u_offset * vec2(1.0, -1.0);
-        gl_Position = vec4(cpos.xy / cpos.w + (offset + dir * u_radius) / u_resolution * 2.0, 0.0, 1.0);
+        gl_Position = vec4(cpos.xy / cpos.w + (offset + v_position) / u_resolution * 2.0, 0.0, 1.0);
     }
-
-    vDir = dir * u_radius;
 }
