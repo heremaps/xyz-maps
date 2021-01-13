94ec0fb495ba6608d88d4034e36937b8b62c0dc9
Tim Deubler <tim.deubler@here.com>
2021-01-13 18:22:54 +0100

    removed obsolete/legacy docs package
    Signed-off-by: Tim Deubler <tim.deubler@here.com>

--- packages/docs/clean.js
+++
@@ -1,8 +0,0 @@
-let rimraf = require('rimraf');
-let argv = require('yargs').argv;
-
-let config = require(argv.configure);
-
-rimraf(config.opts.destination, function(e) {
-    console.log('\x1b[32m%s\x1b[0m', 'Doc cleaned');
-});
