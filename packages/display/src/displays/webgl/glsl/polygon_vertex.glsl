702abf6284af29565552ce3b366fa1bb429bcd54
Tim Deubler <tim.deubler@here.com>
2023-08-18 12:11:14 +0200

    removed polygon tilescale
    Signed-off-by: Tim Deubler <tim.deubler@here.com>

--- packages/display/src/displays/webgl/glsl/polygon_vertex.glsl
+++ packages/display/src/displays/webgl/glsl/polygon_vertex.glsl
@@ -5,8 +5,6 @@ attribute vec3 a_position;
 uniform vec2 u_offsetZ;
 uniform float u_scale;
 uniform float u_zMeterToPixel;
-
-uniform float u_tileScale;
 uniform mat4 u_matrix;
 uniform vec2 u_topLeft;
 
@@ -14,5 +12,5 @@ void main(void){
 
     float z = a_position.z + u_offsetZ.x / u_zMeterToPixel / u_scale;
 
-    gl_Position = u_matrix * vec4( u_topLeft + a_position.xy * u_tileScale, -z, 1.0 );
+    gl_Position = u_matrix * vec4( u_topLeft + a_position.xy, -z, 1.0 );
 }
