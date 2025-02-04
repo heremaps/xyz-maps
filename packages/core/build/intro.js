f8171eaec621a51715f94cd2733c2fbbbc9eb11b
Tim Deubler <tim.deubler@here.com>
2025-02-04 11:28:36 +0100

    updated 2pass worker builds to align with ES2017 factory changes.
    Signed-off-by: Tim Deubler <tim.deubler@here.com>

--- packages/core/build/intro.js
+++ packages/core/build/intro.js
@@ -10,6 +10,6 @@ function define(dep, factory) {
         const sharedExports = {};
         shared(sharedExports);
         xyz.__workerURL = URL.createObjectURL(new Blob(['var shared={};('+shared+')(shared);('+worker+')(shared);'], {type: 'text/javascript'}));
-        factory(xyz, sharedExports, xyz.common);
+        factory(xyz, xyz.common, sharedExports);
     }
 }
