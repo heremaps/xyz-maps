heremaps/xyz-maps

commit f6a6bc8adf0099d4ab9a3618fd26026065966c06

update type casting for results and fix terrain cache imports
Signed-off-by: Tim Deubler <tim.deubler@here.com>

packages/display/src/displays/webgl/GLRender.ts
@@ -1157,10 +1157,13 @@ export class GLRender implements BasicRender {
             id = buffer.progId = Program.getProgramId(buffer, Program.getMacros(buffer));
         }
 
+        console.log(id);
+
         let prog = this.programs[id];
 
         if (prog === undefined) {
             const Program = this.programConfig[type].program;
+            console.log(id, 'create', Program);
             if (Program) {
                 prog = this.createProgram(id, Program, Program.getMacros(buffer));
             }

packages/common/src/TaskSequence.ts
@@ -111,9 +111,9 @@ export class TaskSequence<T extends readonly Task<any, any, any>[]> extends Task
             this.manager._insert(this, true);
             // this.resume();
         } else {
-            const results = this.results.slice();
+            const results = this.results.slice() as unknown as TASK_RESULT<T>;
             this.initSequence();
-            this.onAllDone?.(results as TASK_RESULT<T>[]);
+            this.onAllDone?.(results);
         }
     }
 }

packages/display/src/displays/webgl/buffer/factory/GeometryBufferFactory.ts
@@ -22,7 +22,7 @@ import {Tile} from '@here/xyz-maps-core';
 import {Layer} from '../../../Layers';
 import {FeatureFactory} from '../FeatureFactory';
 import {DisplayTileTask} from '../../../BasicTile';
-import {HeightMapTileCache} from '../../Display';
+import {HeightMapTileCache} from '../../HeightMapTileCache';
 import GLTile from '../../GLTile';
 import {TerrainTask} from './TerrainTask';
 import {FactoryTask, FactoryTaskResult} from './FactoryTask';
@@ -37,7 +37,7 @@ export class GeometryBufferFactory {
         tile: Tile,
         displayTile: GLTile,
         factory: FeatureFactory,
-        terrainCache: TerrainHeightMapCache,
+        terrainCache: HeightMapTileCache,
         gl: WebGLRenderingContext,
         onInit: () => void,
         onDone: (

packages/display/src/displays/webgl/buffer/factory/TerrainTask.ts
@@ -22,7 +22,7 @@ import {GeometryBuffer} from '../GeometryBuffer';
 import GLTile from '../../GLTile';
 import {Texture} from '../../Texture';
 import {TilePreviewInfo} from '../../../Preview';
-import {HeightMapTileCache} from '../../Display';
+import {HeightMapTileCache} from '../../HeightMapTileCache';
 import {Layer} from '../../../Layers';
 
 type TerrainTaskInput = {
@@ -42,7 +42,7 @@ export class TerrainTask extends Task<TerrainTaskInput, TerrainTaskData, HeightM
     name = 'TerrainTask';
     time = 2;
 
-    private terrainCache: TerrainHeightMapCache;
+    private terrainCache: HeightMapTileCache;
 
     private tile: GLTile;
     private terrainLayer: Layer;
@@ -55,7 +55,7 @@ export class TerrainTask extends Task<TerrainTaskInput, TerrainTaskData, HeightM
         gl: WebGLRenderingContext,
         displayTile: GLTile,
         terrainLayer: Layer,
-        terrainCache: TerrainHeightMapCache
+        terrainCache: HeightMapTileCache
     }) {
         super(options);
 

packages/display/src/displays/webgl/program/Heatmap.ts
@@ -22,7 +22,7 @@ import vertexShader from '../glsl/heatmap_vertex.glsl';
 // @ts-ignore
 import fragmentShader from '../glsl/heatmap_fragment.glsl';
 
-import Program from './Program';
+import Program, {CompiledUniformMap} from './Program';
 import {GLStates, PASS} from './GLStates';
 import {Texture} from '../Texture';
 import {GeometryBuffer} from '../buffer/GeometryBuffer';
@@ -160,7 +160,7 @@ class HeatmapProgram extends Program {
             const {offscreenBuffer} = this;
 
             this.initBuffers(offscreenBuffer.attributes);
-            this.initUniforms(offscreenBuffer.uniforms);
+            this.initUniforms(offscreenBuffer.uniforms as CompiledUniformMap);
             this.initAttributes(offscreenBuffer.attributes);
             this.initGeometryBuffer(offscreenBuffer, PASS.ALPHA);
             gl.depthFunc(gl.LEQUAL);

commit cf02a0c668cfc33eb4bd04db0052f10f473761f2

fix global layer render index issues introduced in f6a6bc8
Signed-off-by: Tim Deubler <tim.deubler@here.com>

packages/display/src/displays/Layers.ts
@@ -172,6 +172,10 @@ class Layer {
             dLayer.ready = true;
         }
     }
+
+    getRenderIndex() {
+        return this.index + 1;
+    }
 }
 
 class Layers extends Array<Layer> {

packages/display/src/displays/webgl/Display.ts
@@ -474,10 +474,7 @@ class WebGlDisplay extends BasicDisplay {
         for (let buffer of buffers) {
             let {zLayer, zIndex} = buffer;
 
-            if (zLayer == null) {
-                zLayer = layer.index + 1;
-            }
-
+            zLayer ??= layer.getRenderIndex();
             let z = zLayer * 1e8 + zIndex * 10;
 
             if (!buffer.flat) {

packages/display/src/displays/webgl/GLRender.ts
@@ -1157,8 +1157,6 @@ export class GLRender implements BasicRender {
             id = buffer.progId = Program.getProgramId(buffer, Program.getMacros(buffer));
         }
 
-        console.log(id);
-
         let prog = this.programs[id];
 
         if (prog === undefined) {

packages/display/src/displays/webgl/buffer/FeatureFactory.ts
@@ -192,7 +192,7 @@ export class FeatureFactory {
         this.groups = groups;
         this.tileSize = tileSize;
         this.z = zoom;
-        this.zLayer = zLayer ?? -1;
+        this.zLayer = zLayer;
         this.lineFactory.initTile();
         this.pendingCollisions.length = 0;
         this.waitAndRefresh = waitAndRefresh;
@@ -1176,7 +1176,6 @@ export class FeatureFactory {
 
             this.pendingCollisions.push(collisionGrp);
         }
-        ;
     }
 
     destroy() {

packages/display/src/displays/webgl/buffer/factory/FactoryTask.ts
@@ -149,9 +149,10 @@ export class FactoryTask extends Task<TaskInput, TaskData> {
         const waitAndRefresh = (promise: Promise<any>) => {
             pendingResources.push(promise);
         };
-        this.factory.init(tile, groups, layer.tileSize, zoom, layerStyles.zLayer, waitAndRefresh);
-
         const {showWireframe} = layerStyles;
+        const zLayer = layerStyles.zLayer ?? displayLayer.getRenderIndex();
+
+        this.factory.init(tile, groups, layer.tileSize, zoom, zLayer, waitAndRefresh);
 
         return {
             tile,
@@ -458,11 +459,6 @@ export class FactoryTask extends Task<TaskInput, TaskData> {
             }
         }
         // console.log('buffers', buffers);
-
-        for (let b of buffers) {
-            b.id = b.type + ':' + tile.quadkey;
-        }
-
         return taskData.onDone(buffers.reverse());
     };
 